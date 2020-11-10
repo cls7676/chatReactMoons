35c6eda7482474db650d7c967dba859f53b23d00
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-11-10 17:51:11 +0000

    irq_remap: disable IRQ remapping if any IOAPIC lacks an IOMMU

---
+++ dotnet/src/SemanticKernel/AI/AIException.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using Microsoft.SemanticKernel.Diagnostics;
+
+namespace Microsoft.SemanticKernel.AI;
+
+/// <summary>
+/// AI logic exception
+/// </summary>
+public class AIException : Exception<AIException.ErrorCodes>
+{
+    /// <summary>
+    /// Possible error codes for exceptions
+    /// </summary>
+    public enum ErrorCodes
+    {
+        /// <summary>
+        /// Unknown error.
+        /// </summary>
+        UnknownError = -1,
+
+        /// <summary>
+        /// No response.
+        /// </summary>
+        NoResponse,
+
+        /// <summary>
+        /// Access is denied.
+        /// </summary>
+        AccessDenied,
+
+        /// <summary>
+        /// The request was invalid.
+        /// </summary
