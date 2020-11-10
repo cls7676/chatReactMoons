61d8fe1964f377359f25c2970a08c2ff55713733
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-11-10 19:28:15 +0000

    #89

--- dotnet/src/SemanticKernel/AI/Embeddings/IEmbeddingGenerator.cs
+++ dotnet/src/SemanticKernel/AI/Embeddings/IEmbeddingGenerator.cs
@@ -23,4 +23,24 @@ public interface IEmbeddingGenerator<TValue, TEmbedding>
     Task<IList<Embedding<TEmbedding>>> GenerateEmbeddingsAsync(IList<TValue> data);
 }
 
-/// <summ
+/// <summary>
+/// Provides a collection of static methods for operating on <see cref="IEmbeddingGenerator{TValue, TEmbedding}"/> objects.
+/// </summary>
+public static class EmbeddingGeneratorExtensions
+{
+    /// <summary>
+    /// Generates an embedding from the given <paramref name="value"/>.
+    /// </summary>
+    /// <typeparam name="TValue">The type from which embeddings will be generated.</typeparam>
+    /// <typeparam name="TEmbedding">The numeric type of the embedding data.</typeparam>
+    /// <param name="generator">The embedding generator.</param>
+    /// <param name="value">A value from which an <see cref="Embedding{TEmbedding}"/> will be generated.</param>
+    /// <returns>A list of <see cref="Embedding{TEmbedding}"/> structs representing the input <paramref name="value"/>.</returns>
+    public static async Task<Embedding<TEmbedding>> GenerateEmbeddingAsync<TValue, TEmbedding>
+        (this IEmbeddingGenerator<TValue, TEmbedding> generator, TValue value)
+        where TEmbedding : unmanaged
+    {
+        Verify.NotNull(generator, "Embeddings generator cannot be NULL");
+        return (await generator.GenerateEmbeddingsAsync(new[] { value })).FirstOrDefault();
+    }
+}
---
+++ dotnet/src/SemanticKernel/AI/Embeddings/IEmbeddingIndex.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.Diagnostics;
+
+namespace Microsoft.SemanticKernel.AI.Embeddings;
+
+/// <summary>
+/// Represents an searchable index of <see cref="Embedding{TEmbedding}"/> structs.
+/// </summary>
+/// <typeparam name="TEmbedding">The data type of the embedding.</typeparam>
+public interface IEmbeddingIndex<TEmbedding>
+    where TEmbedding : unmanaged
+{
+    /// <summary>
+    /// Gets the nearest matches to the <see cref="Embedding{TEmbedding}"/>.
+    /// </summary>
+    /// <param name="collection">The storage collection to search.</param>
+    /// <param name="embedding">The input <see cref="Embedding{TEmbedding}"/> to use as the search.</param>
+    /// <param name="limit">The max number of results to return.</param>
+    /// <param name="minRelevanceScore">The minimum score to consider in the distance calculation.</param>
+    /// <returns>A tuple consisting of the <see
