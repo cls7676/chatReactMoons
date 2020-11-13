260e055991bf82133ca3d22fb985dd9cdf7e7087
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-11-13 22:55:12 +0000

    Update factory

--- dotnet/src/SemanticKernel/CoreSkills/TextSkill.cs
+++ dotnet/src/SemanticKernel/CoreSkills/TextSkill.cs
@@ -57,4 +57,43 @@ public class TextSkill
     /// </summary>
     /// <example>
     /// SKContext["input"] = "  hello world  "
-    /// {{te
+    /// {{text.trimEnd $input} => "  hello world"
+    /// </example>
+    /// <param name="text"> The string to trim. </param>
+    /// <returns> The trimmed string. </returns>
+    [SKFunction("Trim whitespace from the end of a string.")]
+    public string TrimEnd(string text)
+    {
+        return text.TrimEnd();
+    }
+
+    /// <summary>
+    /// Convert a string to uppercase.
+    /// </summary>
+    /// <example>
+    /// SKContext["input"] = "hello world"
+    /// {{text.uppercase $input}} => "HELLO WORLD"
+    /// </example>
+    /// <param name="text"> The string to convert. </param>
+    /// <returns> The converted string. </returns>
+    [SKFunction("Convert a string to uppercase.")]
+    public string Uppercase(string text)
+    {
+        return text.ToUpper(System.Globalization.CultureInfo.CurrentCulture);
+    }
+
+    /// <summary>
+    /// Convert a string to lowercase.
+    /// </summary>
+    /// <example>
+    /// SKContext["input"] = "HELLO WORLD"
+    /// {{text.lowercase $input}} => "hello world"
+    /// </example>
+    /// <param name="text"> The string to convert. </param>
+    /// <returns> The converted string. </returns>
+    [SKFunction("Convert a string to lowercase.")]
+    public string Lowercase(string text)
+    {
+        return text.ToLower(System.Globalization.CultureInfo.CurrentCulture);
+    }
+}
---
+++ dotnet/src/SemanticKernel/CoreSkills/TimeSkill.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Globalization;
+using Microsoft.SemanticKernel.SkillDefinition;
+
+namespace Microsoft.SemanticKernel.CoreSkills;
+
+/// <summary>
+/// TimeSkill provides a set of functions to get the current time and date.
+/// </summary>
+/// <example>
+/// Usage: kernel.ImportSkill("time", new TimeSkill());
+/// Examples:
+/// {{time.date}}            => Sunday, 12 January, 2031
+/// {{time.today}}           => Sunday, 12 January, 2031
+/// {{time.now}}             => Sunday, January 12, 2031 9:15 PM
+/// {{time.utcNow}}          => Sunday, January 13, 2031 5:15 AM
+/// {{time.time}}            => 09:15:07 PM
+/// {{time.year}}            => 2031
+/// {{time.month}}           => January
+/// {{time.monthNumber}}     => 01
+/// {{time.day}}             => 12
+/// {{time.dayOfMonth}}      => 12
+/// {{time.dayOfWeek}}       => Sunday
+/// {{time.hour}}            => 9 PM
+/// {{time.hourNumber}}      => 21
+/// {{ti
