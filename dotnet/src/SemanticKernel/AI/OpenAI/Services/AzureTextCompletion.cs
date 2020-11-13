e1953c63c1b8fa18844e620ac8f64f1b6ffc281a
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-11-13 11:02:22 +0000

    Update issue and PR templates

---
+++ dotnet/src/SemanticKernel/AI/OpenAI/Services/AzureTextCompletion.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.AI.OpenAI.Clients;
+using Microsoft.SemanticKernel.AI.OpenAI.HttpSchema;
+using Microsoft.SemanticKernel.Diagnostics;
+usin
