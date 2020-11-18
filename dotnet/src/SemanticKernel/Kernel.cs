2304760c74b843f9675905fad2cd98f9ac39b45e
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-11-18 18:55:48 +0000

    Fix CS/WS issue

--- dotnet/src/SemanticKernel/Kernel.cs
+++ dotnet/src/SemanticKernel/Kernel.cs
@@ -108,4 +108,67 @@ public sealed class Kernel : IKernel, IDisposable
             this._log.LogTrace("Importing skill {0}", skillName);
         }
 
-        var skill = new Dictionary<string, ISKFunction>(St
+        var skill = new Dictionary<string, ISKFunction>(StringComparer.OrdinalIgnoreCase);
+        IEnumerable<ISKFunction> functions = ImportSkill(skillInstance, skillName, this._log);
+        foreach (ISKFunction f in functions)
+        {
+            f.SetDefaultSkillCollection(this.Skills);
+            this._skillCollection.AddNativeFunction(f);
+            skill.Add(f.Name, f);
+        }
+
+        return skill;
+    }
+
+    /// <inheritdoc/>
+    public void RegisterMemory(ISemanticTextMemory memory)
+    {
+        this._memory = memory;
+    }
+
+    /// <inheritdoc/>
+    public Task<SKContext> RunAsync(params ISKFunction[] pipeline)
+        => this.RunAsync(new ContextVariables(), pipeline);
+
+    /// <inheritdoc/>
+    public Task<SKContext> RunAsync(string input, params ISKFunction[] pipeline)
+        => this.RunAsync(new ContextVariables(input), pipeline);
+
+    /// <inheritdoc/>
+    public Task<SKContext> RunAsync(ContextVariables variables, params ISKFunction[] pipeline)
+        => this.RunAsync(variables, CancellationToken.None, pipeline);
+
+    /// <inheritdoc/>
+    public Task<SKContext> RunAsync(CancellationToken cancellationToken, params ISKFunction[] pipeline)
+        => this.RunAsync(new ContextVariables(), cancellationToken, pipeline);
+
+    /// <inheritdoc/>
+    public Task<SKContext> RunAsync(string input, CancellationToken cancellationToken, params ISKFunction[] pipeline)
+        => this.RunAsync(new ContextVariables(input), cancellationToken, pipeline);
+
+    /// <inheritdoc/>
+    public async Task<SKContext> RunAsync(ContextVariables variables, CancellationToken cancellationToken, params ISKFunction[] pipeline)
+    {
+        var context = new SKContext(
+            variables,
+            this._memory,
+            this._skillCollection.ReadOnlySkillCollection,
+            this._log,
+            cancellationToken);
+
+        int pipelineStepCount = -1;
+        foreach (ISKFunction f in pipeline)
+        {
+            if (context.ErrorOccurred)
+            {
+                this._log.LogError(
+                    context.LastException,
+                    "Something went wrong in pipeline step {0}:'{1}'", pipelineStepCount, context.LastErrorDescription);
+                return context;
+            }
+
+            pipelineStepCount++;
+
+            try
+            {
+                cancellationToken.ThrowIfCancellati
