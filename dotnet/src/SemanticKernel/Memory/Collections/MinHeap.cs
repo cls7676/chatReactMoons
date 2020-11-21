595aaa79867d8af86f153f374a925ed5ac6d43c5
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-11-21 20:42:08 +0000

    Change layout

--- dotnet/src/SemanticKernel/Memory/Collections/MinHeap.cs
+++ dotnet/src/SemanticKernel/Memory/Collections/MinHeap.cs
@@ -65,4 +65,91 @@ internal class MinHeap<T> : IEnumerable<T> where T : IComparable<T>
 
     public bool IsEmpty => (this._count == 0);
 
-    public void C
+    public void Clear()
+    {
+        this._count = 0;
+    }
+
+    public void Erase()
+    {
+        Array.Clear(this._items, 1, this._count);
+        this._count = 0;
+    }
+
+    public T[] DetachBuffer()
+    {
+        T[] buf = this._items;
+        this._items = s_emptyBuffer;
+        this._count = 0;
+        return buf;
+    }
+
+    public void Add(T item)
+    {
+        //
+        // the 0'th item is always a sentinal and not included in this._count. 
+        // The length of the buffer is always this._count + 1
+        //
+        this._count++;
+        this.EnsureCapacity();
+        this._items[this._count] = item;
+        this.UpHeap(this._count);
+    }
+
+    public void Add(IEnumerable<T> items)
+    {
+        foreach (T item in items)
+        {
+            this.Add(item);
+        }
+    }
+
+    public void Add(IList<T> items, int startAt = 0)
+    {
+        Verify.NotNull(items, nameof(items));
+
+        int newItemCount = items.Count;
+
+        Verify.LessThan(startAt, newItemCount, $"{nameof(startAt)} value must be less than {nameof(items)} count.");
+
+        this.EnsureCapacity(this._count + (newItemCount - startAt));
+        for (int i = startAt; i < newItemCount; ++i)
+        {
+            //
+            // the 0'th item is always a sentinal and not included in this._count. 
+            // The length of the buffer is always this._count + 1
+            //
+            this._count++;
+            this._items[this._count] = items[i];
+            this.UpHeap(this._count);
+        }
+    }
+
+    public T RemoveTop()
+    {
+        if (this._count == 0)
+        {
+            throw new InvalidOperationException("MinHeap is empty.");
+        }
+
+        T item = this._items[1];
+        this._items[1] = this._items[this._count--];
+        this.DownHeap(1);
+        return item;
+    }
+
+    public IEnumerable<T> RemoveAll()
+    {
+        while (this._count > 0)
+        {
+            yield return this.RemoveTop();
+        }
+    }
+
+    public void EnsureCapacity(int capacity)
+    {
+        Verify.GreaterThan(capacity, MinCapacity, $"MinHeap capacity must be greater than {MinCapacity}.");
+
+        // 0th item is always a sentinal
+        capacity++;
+        if (capacit
