28036733e04065dc4271790422aacee953503287
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-11-24 13:13:25 +0000

    changes added

---
+++ dotnet/src/SemanticKernel/Memory/SemanticTextMemory.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.AI.Embeddings;
+using Microsoft.SemanticKernel.Memory.Storage;
+
+namespace Microsoft.SemanticKernel.Memory;
+
+/// <summary>
+/// Implementation of <see cref="ISemanticTextMemory"/>./>.
+/// </summary>
+public sealed class SemanticTextMemory : ISemanticTextMemory, IDisposable
+{
+    private readonly IEmbeddingGenerator<string, float> _embeddingGenerator;
+    private readonly IMemoryStore<float> _storage;
+
+    public Sem
