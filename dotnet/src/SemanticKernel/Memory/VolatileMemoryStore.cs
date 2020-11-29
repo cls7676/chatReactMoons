35cd60d66e95e8edb93d1b6ac4f338498912ff49
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-11-29 09:16:01 +0000

    more changes to readme

--- dotnet/src/SemanticKernel/Memory/VolatileMemoryStore.cs
+++ dotnet/src/SemanticKernel/Memory/VolatileMemoryStore.cs
@@ -6,3 +6,63 @@ using Microsoft.SemanticKernel.AI.Embeddings;
 using Microsoft.SemanticKernel.AI.Embeddings.VectorOperations;
 using Microsoft.SemanticKernel.Memory.Collections;
 using Microsoft.SemanticKernel.Memory.Storage;
+
+namespace Microsoft.SemanticKernel.Memory;
+
+/// <summary>
+/// A simple volatile memory embeddings store.
+/// TODO: multiple enumerations
+/// </summary>
+/// <typeparam name="TEmbedding">Embedding type</typeparam>
+public class VolatileMemoryStore<TEmbedding> : VolatileDataStore<IEmbeddingWithMetadata<TEmbedding>>, IMemoryStore<TEmbedding>
+    where TEmbedding : unmanaged
+{
+    /// <inheritdoc/>
+    public IAsyncEnumerable<(IEmbeddingWithMetadata<TEmbedding>, double)> GetNearestMatchesAsync(
+        string collection,
+        Embedding<TEmbedding> embedding,
+        int limit = 1,
+        double minRelevanceScore = 0)
+    {
+        if (limit <= 0)
+        {
+            return AsyncEnumerable.Empty<(IEmbeddingWithMetadata<TEmbedding>, double)>();
+        }
+
+        IEnumerable<DataEntry<IEmbeddingWithMetadata<TEmbedding>>>? embeddingCollection = null;
+        if (this.TryGetCollection(collection, out var collectionDict))
+        {
+            embeddingCollection = collectionDict.Values;
+        }
+
+        if (embeddingCollection == null || !embeddingCollection.Any())
+        {
+            return AsyncEnumerable.Empty<(IEmbeddingWithMetadata<TEmbedding>, double)>();
+        }
+
+        EmbeddingReadOnlySpan<TEmbedding> embeddingSpan = new(embedding.AsReadOnlySpan());
+
+        TopNCollection<IEmbeddingWithMetadata<TEmbedding>> embeddings = new(limit);
+
+        foreach (var item in embeddingCollection)
+        {
+            if (item.Value != null)
+            {
+                EmbeddingReadOnlySpan<TEmbedding> itemSpan = new(item.Value.Embedding.AsReadOnlySpan());
+                double similarity = embeddingSpan.CosineSimilarity(itemSpan);
+                if (similarity >= minRelevanceScore)
+                {
+                    embeddings.Add(new(item.Value, similarity));
+                }
+            }
+        }
+
+        embeddings.SortByScore();
+
+        return embeddings.Select(x => (x.Value, x.Score.Value)).ToAsyncEnumerable();
+    }
+
+    #region private ================================================================================
+
+    /// <summary>
+    /// Calculates the cosine similarity between an <see cr
