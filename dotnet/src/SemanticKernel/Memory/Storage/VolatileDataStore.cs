b7824785713e63bc34280a39f8c0493d6324d6a9
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-11-29 08:43:04 +0000

    remove email

--- dotnet/src/SemanticKernel/Memory/Storage/VolatileDataStore.cs
+++ dotnet/src/SemanticKernel/Memory/Storage/VolatileDataStore.cs
@@ -37,4 +37,65 @@ public class VolatileDataStore<TValue> : IDataStore<TValue>
 
     /// <inheritdoc/>
     public virtual Task<DataEntry<TValue>?> GetAsync(string collection, string key, CancellationToken cancel = default)
- 
+    {
+        if (this.TryGetCollection(collection, out var collectionDict)
+            && collectionDict.TryGetValue(key, out var dataEntry))
+        {
+            return Task.FromResult<DataEntry<TValue>?>(dataEntry);
+        }
+
+        return Task.FromResult<DataEntry<TValue>?>(null);
+    }
+
+    /// <inheritdoc/>
+    public virtual Task<DataEntry<TValue>> PutAsync(string collection, DataEntry<TValue> data, CancellationToken cancel = default)
+    {
+        Verify.NotNull(data, "Data entry cannot be NULL");
+
+        if (this.TryGetCollection(collection, out var collectionDict, create: true))
+        {
+            collectionDict[data.Key] = data;
+        }
+
+        return Task.FromResult(data);
+    }
+
+    /// <inheritdoc/>
+    public virtual Task RemoveAsync(string collection, string key, CancellationToken cancel = default)
+    {
+        if (this.TryGetCollection(collection, out var collectionDict))
+        {
+            collectionDict.Remove(key, out DataEntry<TValue> _);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    #region protected ================================================================================
+
+    protected bool TryGetCollection(string name, [NotNullWhen(true)] out ConcurrentDictionary<string, DataEntry<TValue>>? collection, bool create = false)
+    {
+        if (this._store.TryGetValue(name, out collection))
+        {
+            return true;
+        }
+
+        if (create)
+        {
+            collection = new ConcurrentDictionary<string, DataEntry<TValue>>();
+            return this._store.TryAdd(name, collection);
+        }
+
+        collection = null;
+        return false;
+    }
+
+    #endregion
+
+    #region private ================================================================================
+
+    private readonly ConcurrentDictionary<string,
+        ConcurrentDictionary<string, DataEntry<TValue>>> _store = new();
+
+    #endregion
+}
---
+++ dotnet/src/SemanticKernel/Memory/VolatileMemoryStore.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.SemanticKernel.AI.Embeddings;
+using Microsoft.SemanticKernel.AI.Embeddings.VectorOperations;
+using Microsoft.SemanticKernel.Memory.Collections;
+using Microsoft.SemanticKernel.Memory.Storage;
