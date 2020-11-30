5680e34f16a67dc20e979575624904d147fa3a22
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-11-30 17:30:00 +0000

    renamed modules

--- dotnet/src/SemanticKernel/SemanticFunctions/Partitioning/SemanticTextPartitioner.cs
+++ dotnet/src/SemanticKernel/SemanticFunctions/Partitioning/SemanticTextPartitioner.cs
@@ -18,4 +18,60 @@ public static class SemanticTextPartitioner
     /// Split plain text into lines.
     /// </summary>
     /// <param name="text">Text to split</param>
-    /// <param name="maxTokensPerLine">Maximum number of tokens p
+    /// <param name="maxTokensPerLine">Maximum number of tokens per line.</param>
+    /// <returns>List of lines.</returns>
+    public static List<string> SplitPlainTextLines(string text, int maxTokensPerLine)
+    {
+        return InternalSplitPlaintextLines(text, maxTokensPerLine, true);
+    }
+
+    /// <summary>
+    /// Split markdown text into lines.
+    /// </summary>
+    /// <param name="text">Text to split</param>
+    /// <param name="maxTokensPerLine">Maximum number of tokens per line.</param>
+    /// <returns>List of lines.</returns>
+    public static List<string> SplitMarkDownLines(string text, int maxTokensPerLine)
+    {
+        return InternalSplitMarkdownLines(text, maxTokensPerLine, true);
+    }
+
+    /// <summary>
+    /// Split plain text into paragraphs.
+    /// </summary>
+    /// <param name="lines">Lines of text.</param>
+    /// <param name="maxTokensPerParagraph">Maximum number of tokens per paragraph.</param>
+    /// <returns>List of paragraphs.</returns>
+    public static List<string> SplitPlainTextParagraphs(List<string> lines, int maxTokensPerParagraph)
+    {
+        return InternalSplitTextParagraphs(lines, maxTokensPerParagraph, text => InternalSplitPlaintextLines(text, maxTokensPerParagraph, false));
+    }
+
+    /// <summary>
+    /// Split markdown text into paragraphs.
+    /// </summary>
+    /// <param name="lines">Lines of text.</param>
+    /// <param name="maxTokensPerParagraph">Maximum number of tokens per paragraph.</param>
+    /// <returns>List of paragraphs.</returns>
+    public static List<string> SplitMarkdownParagraphs(List<string> lines, int maxTokensPerParagraph)
+    {
+        return InternalSplitTextParagraphs(lines, maxTokensPerParagraph, text => InternalSplitMarkdownLines(text, maxTokensPerParagraph, false));
+    }
+
+    private static List<string> InternalSplitTextParagraphs(List<string> lines, int maxTokensPerParagraph, Func<string, List<string>> longLinesSplitter)
+    {
+        if (lines.Count == 0)
+        {
+            return new List<string>();
+        }
+
+        // Split long lines first
+        var truncatedLines = new List<string>();
+        foreach (var line in lines)
+        {
+            truncatedLines.AddRange(longLinesSplitter(line));
+        }
+
+        lines = truncatedLines;
+
+        
