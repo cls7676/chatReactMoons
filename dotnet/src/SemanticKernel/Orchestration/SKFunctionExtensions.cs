a93333a35a500c98c2c69ec219e2c6a583f5d440
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-11-30 10:16:39 +0000

    updated screenshots

--- dotnet/src/SemanticKernel/Orchestration/SKFunctionExtensions.cs
+++ dotnet/src/SemanticKernel/Orchestration/SKFunctionExtensions.cs
@@ -93,4 +93,30 @@ public static class SKFunctionExtensions
     /// </summary>
     /// <param name="function">Function to execute</param>
     /// <param name="input">Custom function input</param>
-    /// <param name="memory">Semantic memory</
+    /// <param name="memory">Semantic memory</param>
+    /// <param name="skills">Available skills</param>
+    /// <param name="log">App logger</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The temporary context</returns>
+    public static async Task<SKContext> InvokeWithCustomInputAsync(this ISKFunction function,
+        ContextVariables input,
+        ISemanticTextMemory memory,
+        IReadOnlySkillCollection? skills,
+        ILogger log,
+        CancellationToken cancellationToken)
+    {
+        var tmpContext = new SKContext(input, memory, skills, log, cancellationToken);
+        try
+        {
+            await function.InvokeAsync(tmpContext);
+        }
+        catch (Exception ex) when (!ex.IsCriticalException())
+        {
+            log.LogError(ex, "Something went wrong when invoking function with custom input: {0}.{1}. Error: {2}", function.SkillName,
+                function.Name, ex.Message);
+            tmpContext.Fail(ex.Message, ex);
+        }
+
+        return tmpContext;
+    }
+}
---
+++ dotnet/src/SemanticKernel/Planning/PlanningException.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using Microsoft.SemanticKernel.Diagnostics;
+
+namespace Microsoft.SemanticKernel.Planning;
+
+/// <summary>
+/// Planning exception.
+/// </summary>
+public class PlanningException : Exception<PlanningException.ErrorCodes>
+{
+    /// <summary>
+    /// Error codes for <see cref="PlanningException"/>.
+    /// </summary>
+    public enum ErrorCodes
+    {
+        /// <summary>
+        /// Unknown error.
+        /// </summary>
+        UnknownError = -1,
+
+        /// <summary>
+        /// Invalid plan.
+        /// </summary>
+        InvalidPlan = 0,
+
+        /// <summary>
+        /// In
