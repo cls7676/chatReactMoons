84446ad68845eaecc60bd4aafb2d3da6395521e5
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-12-01 16:57:56 +0000

    Bump version post-release

---
+++ dotnet/src/SemanticKernel/TemplateEngine/Blocks/TextBlock.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.Orchestration;
+
+namespace Microsoft.SemanticKernel.TemplateEngine.Bl
