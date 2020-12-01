2a854ce88024b0e0f73707d31c9667d4bfd9b149
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-12-01 18:22:56 +0000

    First version.

---
+++ dotnet/src/SemanticKernel/TemplateEngine/PromptTemplateEngine.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.TemplateEngine.Blocks;
+
+namespace Microsoft.SemanticKernel.TemplateEngine;
+
+/// <summary>
+/// Given a prompt, that might contain references to variables and functions:
+/// - Get the list of references
+/// - Resolve each reference
+///   - Variable references are resolved using the context variables
+///   - Function references are resolved invoking those functions
+///     - Functions can be invoked passing in variables
+///     - Functions do not receive the context variables, unless specified using a special variable
+///     - Functions can be invoked in order and in parallel so the context variables must be immutable when invoked within the template
+/// </summary>
+public class PromptTemplateEngine : IPromptTemplateEngine
+{
+    public PromptTemplateEngine(ILogger? log = null)
+    {
+        this._log = log ?? NullLogger.Instance;
+    }
+
+    /// <summary>
+    /// Given a prompt template string, extract all the blocks (text, variables, function calls)
+    /// </summary>
+    /// <param name="templateText">Prompt template (see skprompt.txt files)</param>
+    /// <param name="validate">Whether to validate the blocks sy
