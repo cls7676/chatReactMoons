90432699b1187ef09151e3e66c717c0d76234899
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-12-02 14:23:41 +0000

    Set up CI with Azure Pipelines

---
+++ dotnet/src/SemanticKernel.IntegrationTests/AI/OpenAIEmbeddingTests.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.AI.Embeddings;
+using Microsoft.SemanticKernel.AI.OpenAI.Services;
+using SemanticKernel.IntegrationTests.TestSettings;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SemanticKernel.IntegrationTests.AI;
+
+public sealed class OpenAIEmbeddingTests : IDisposable
+{
+    private const int AdaVectorLength = 1536;
+    private readonly IConfigurationRoot _configuration;
+
+    public OpenAIEmbeddingTests(ITestOutputHelper output)
+    {
+        this._testOutputHelper = new RedirectOutput(output);
+        Console.SetOut(this._testOutputHelper);
+
+        // Load configuration
+        this._configuration = new ConfigurationBuilder()
+   
--- dotnet/src/SemanticKernel/Text/Json.cs
+++ dotnet/src/SemanticKernel/Text/Json.cs
@@ -2,4 +2,35 @@
 
 using System.Text.Json;
 
-namespace Microsoft.SemanticKernel.Text
+namespace Microsoft.SemanticKernel.Text;
+
+internal static class Json
+{
+    internal static string Serialize(object? o)
+    {
+        return JsonSerializer.Serialize(o, s_options);
+    }
+
+    internal static T? Deserialize<T>(string json)
+    {
+        return JsonSerializer.Deserialize<T>(json, s_options);
+    }
+
+    internal static string ToJson(this object o)
+    {
+        return JsonSerializer.Serialize(o, s_options);
+    }
+
+    #region private ================================================================================
+
+    private static readonly JsonSerializerOptions s_options = new()
+    {
+        WriteIndented = true,
+        MaxDepth = 20,
+        AllowTrailingCommas = true,
+        PropertyNameCaseInsensitive = true,
+        ReadCommentHandling = JsonCommentHandling.Skip,
+    };
+
+    #endregion
+}
