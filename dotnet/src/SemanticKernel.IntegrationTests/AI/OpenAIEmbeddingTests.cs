1ac92c5ea8b88f68dd82d66b9feeeb66cafce076
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-12-02 15:12:41 +0000

    Shrink ashmem directly through shmem_fallocate

--- dotnet/src/SemanticKernel.IntegrationTests/AI/OpenAIEmbeddingTests.cs
+++ dotnet/src/SemanticKernel.IntegrationTests/AI/OpenAIEmbeddingTests.cs
@@ -24,4 +24,58 @@ public sealed class OpenAIEmbeddingTests : IDisposable
 
         // Load configuration
         this._configuration = new ConfigurationBuilder()
-   
+            .AddJsonFile(path: "testsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile(path: "testsettings.development.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .AddUserSecrets<OpenAIEmbeddingTests>()
+            .Build();
+    }
+
+    [Theory(Skip = "OpenAI will often throttle requests. This test is for manual verification.")]
+    [InlineData("test sentence")]
+    public async Task OpenAITestAsync(string testInputString)
+    {
+        // Arrange
+        OpenAIConfiguration? openAIConfiguration = this._configuration.GetSection("OpenAIEmbeddings").Get<OpenAIConfiguration>();
+        Assert.NotNull(openAIConfiguration);
+
+        OpenAITextEmbeddings embeddingGenerator = new OpenAITextEmbeddings(openAIConfiguration.ModelId, openAIConfiguration.ApiKey);
+
+        // Act
+        var singleResult = await embeddingGenerator.GenerateEmbeddingAsync(testInputString);
+        var batchResult = await embeddingGenerator.GenerateEmbeddingsAsync(new List<string> { testInputString, testInputString, testInputString });
+
+        // Assert
+        Assert.Equal(AdaVectorLength, singleResult.Count);
+        Assert.Equal(3, batchResult.Count);
+
+        embeddingGenerator.Dispose();
+    }
+
+    [Theory]
+    [InlineData("test sentence")]
+    public async Task AzureOpenAITestAsync(string testInputString)
+    {
+        // Arrange
+        AzureOpenAIConfiguration? azureOpenAIConfiguration = this._configuration.GetSection("AzureOpenAIEmbeddings").Get<AzureOpenAIConfiguration>();
+        Assert.NotNull(azureOpenAIConfiguration);
+
+        AzureTextEmbeddings embeddingGenerator = new AzureTextEmbeddings(azureOpenAIConfiguration.DeploymentName,
+            azureOpenAIConfiguration.Endpoint,
+            azureOpenAIConfiguration.ApiKey,
+            "2022-12-01");
+
+        // Act
+        var singleResult = await embeddingGenerator.GenerateEmbeddingAsync(testInputString);
+        var batchResult = await embeddingGenerator.GenerateEmbeddingsAsync(new List<string> { testInputString, testInputString, testInputString });
+
+        // Assert
+        Assert.Equal(AdaVectorLength, singleResult.Count);
+        Assert.Equal(3, batchResult.Count);
+
+        embeddingGenerator.Dispose();
+    }
+
+    #region internals
+
+    private readonly RedirectOutput _testOutputHelper
