acfd235e4ea5cbffe21feb654740b8251121167b
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-12-03 10:26:39 +0000

    Update le4ispc.sh

--- dotnet/src/SemanticKernel.Skills/Skills.Document/FileSystem/LocalFileSystemConnector.cs
+++ dotnet/src/SemanticKernel.Skills/Skills.Document/FileSystem/LocalFileSystemConnector.cs
@@ -36,4 +36,40 @@ public class LocalFileSystemConnector : IFileSystemConnector
     /// </summary>
     /// <param name="filePath">Path to file</param>
     /// <param name="cancellationToken">Cancellation token.</param>
-    /// <exception cref="ArgumentExcept
+    /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="PathTooLongException"></exception>
+    /// <exception cref="DirectoryNotFoundException"></exception>
+    /// <exception cref="IOException"></exception>
+    /// <exception cref="UnauthorizedAccessException"></exception>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    /// <exception cref="FileNotFoundException"></exception>
+    /// <exception cref="NotSupportedException"></exception>
+    public Task<Stream> GetWriteableFileStreamAsync(string filePath, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult<Stream>(File.Open(Environment.ExpandEnvironmentVariables(filePath), FileMode.Open, FileAccess.ReadWrite));
+    }
+
+    /// <summary>
+    /// Get a writeable stream to a file.
+    /// </summary>
+    /// <param name="filePath">Path to file</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="PathTooLongException"></exception>
+    /// <exception cref="DirectoryNotFoundException"></exception>
+    /// <exception cref="IOException"></exception>
+    /// <exception cref="UnauthorizedAccessException"></exception>
+    /// <exception cref="NotSupportedException"></exception>
+    public Task<Stream> CreateFileAsync(string filePath, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult<Stream>(File.Create(Environment.ExpandEnvironmentVariables(filePath)));
+    }
+
+    /// <inheritdoc/>
+    public Task<bool> FileExistsAsync(string filePath, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(File.Exists(Environment.ExpandEnvironmentVariables(filePath)));
+    }
+}
---
+++ dotnet/src/SemanticKernel.Skills/Skills.Document/IDocumentConnector.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.IO;
+
+namespace Microsoft.SemanticKernel.Skills.Document;
+
+/// <summary>
+/// Interface for document connections (e.g. Microsoft Word)
+/// </summary>
+public interface IDocumentConnector
+{
+    /// <summary>
+    /// Read all text from the document.
+    /// </summary>
+    /// <param name="stream">Document stream</param>
+    /// <returns>Str
