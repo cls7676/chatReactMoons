19a81919639c2cf2070925ed7a1a518930164f24
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-12-04 20:00:01 +0000

    Minor README update

--- dotnet/src/SemanticKernel.Skills/Skills.Document/IDocumentConnector.cs
+++ dotnet/src/SemanticKernel.Skills/Skills.Document/IDocumentConnector.cs
@@ -13,4 +13,19 @@ public interface IDocumentConnector
     /// Read all text from the document.
     /// </summary>
     /// <param name="stream">Document stream</param>
-    /// <returns>Str
+    /// <returns>String containing all text from the document.</returns>
+    public string ReadText(Stream stream);
+
+    /// <summary>
+    /// Initialize a document from the given stream.
+    /// </summary>
+    /// <param name="stream">IO stream</param>
+    public void Initialize(Stream stream);
+
+    /// <summary>
+    /// Append the specified text to the document.
+    /// </summary>
+    /// <param name="stream">Document stream</param>
+    /// <param name="text">String of text to write to the document.</param>
+    public void AppendText(Stream stream, string text);
+}
