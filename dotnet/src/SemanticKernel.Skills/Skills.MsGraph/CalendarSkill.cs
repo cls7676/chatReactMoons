d0fd6caa183f5e59c81c0dd85487203ccb4de7fa
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-12-05 09:50:16 +0000

    remove unused use statement

---
+++ dotnet/src/SemanticKernel.Skills/Skills.MsGraph/CalendarSkill.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.SkillDefinition;
+using Microsoft.SemanticKernel.Skills.MsGraph.Diagnostics;
+using Microsoft.SemanticKernel.Skills.MsGraph.Models;
+
+namespace Microsoft.SemanticKernel.Skills.MsGraph;
+
+/// <summary>
+/// Skill for calendar operations.
+/// </summary>
+public class CalendarSkill
+{
+    /// <summary>
+    /// <see cref="ContextVariables"/> parameter names.
+    /// </summary>
+    public static class Parameters
+    {
+        /// <summary>
+        /// Event start as DateTimeOffs
