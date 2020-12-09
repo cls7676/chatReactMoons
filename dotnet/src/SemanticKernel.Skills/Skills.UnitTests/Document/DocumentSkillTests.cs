25a756c99043f4766c00dacbf5a1402b909bd579
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-12-09 22:14:45 +0000

    Updated the grpc-cpp feedstock.

---
+++ dotnet/src/SemanticKernel.Skills/Skills.UnitTests/Document/DocumentSkillTests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.SemanticKernel.Memory;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.Skills.Document;
+using Microsoft.SemanticKernel.Skills.Document.FileSystem;
+using Moq;
+using Xunit;
+using static Microsoft.SemanticKernel.Skills.Document.DocumentSkill;
+
+namespace SemanticKernel.Skills.UnitTests.Document;
+
+public class DocumentSkillTests
+{
+    private readonly SKContext _context = new(new ContextVariables(), NullMemory.Instance, null, NullLogger.Instance);
+
+    [Fact]
+    public async Task ReadTextAsyncSucceedsAsync()
+    {
+        // Arrange
+        var expectedText = Guid.NewGuid().ToString();
+        var anyFilePath = Guid.NewGuid().ToString();
+
+        var fileSystemConnectorMock = new Mock<IFileSystemConnector>();
+        fileSystemConnectorMock
+            .Setup(mock => mock.GetFileContentStreamAsync(It.Is<string>(filePath => filePath.Equals(anyFilePath, StringComparison.Ordinal)),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Stream.N
