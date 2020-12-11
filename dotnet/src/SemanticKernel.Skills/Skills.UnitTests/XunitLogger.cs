d572e702f0210acd9dc5b8a1aa5ee9c6b603b90e
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-12-11 17:51:25 +0000

    Isha Dubai

---
+++ dotnet/src/SemanticKernel.Skills/Skills.UnitTests/XunitLogger.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace SemanticKernel.Skills.UnitTests;
+
+/// <summary>
+/// A logger that writes to the Xunit test output
+/// </summary>
+internal sealed class XunitLogger<T> : ILogger<T>, IDisposable
+{
+    private readonly ITestOutputHelper _output;
+
+    public XunitLogger(ITestOutputHelper output)
+    {
+        this._output = output;
+    }
+
+    /// <inheritdoc/>
+    public void Lo
