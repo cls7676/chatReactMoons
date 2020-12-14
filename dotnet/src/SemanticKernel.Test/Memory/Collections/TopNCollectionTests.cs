6d999285fe58216ff814a40f158e0589b9118104
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-12-14 09:39:52 +0000

    missed commit

---
+++ dotnet/src/SemanticKernel.Test/Memory/Collections/TopNCollectionTests.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel.Memory.Collections;
+using Xunit;
+
+namespace SemanticKernelTests.Memory.Collections;
+
+/// <summary>
+/// Contains tests for the <see cref="TopNCollection{T}"/> class.
+/// </summary>
+public class TopNCollectionTests
+{
+    private const int MaxItemsCount = 5;
+
+    [Fact]
+    public void ItResetsCollectionCorrectly()
+    {
+        // Arrange
+        const int expectedItemsCount = 0;
+
+        var topNCollection = this.GetTestCollection(MaxItemsCount);
+
+        // Act
+        topNCollection.Reset();
+
+        // Assert
+        Assert.Equal(expectedItemsCount, topNCollection.Count);
+    }
+
+    [Fact]
+    public void ItKeepsMaxItemsCountWhenMoreItemsWereAdded()
+    {
+        // Arrange
+        const int expectedCollectionCount = 5;
+
+        // Act
+        var topNCollection = this.GetTestCollection(expectedCollectionCount);
+
+        // Assert
+        Assert.Equal(expectedCollectionCount, topNCollection.Count);
+    }
+
+    [Fact]
+    public void ItSortsCollectionByScoreInDescendingOrder()
+    {
+        // Arrange
+        var topNCollection =
