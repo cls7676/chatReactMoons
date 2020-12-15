2380aa6d222eac5cdcf8905fbef84ad421916afa
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-12-15 16:15:33 +0000

    UR

--- dotnet/src/SemanticKernel.UnitTests/CoreSkills/PlannerSkillTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/CoreSkills/PlannerSkillTests.cs
@@ -192,4 +192,66 @@ Solve the equation x^2 = 2.
         // Arrange
         var kernel = KernelBuilder.Create();
         _ = kernel.Config.AddOpenAICompletionBackend("test", "test", "test");
-        var plannerSkill = kernel.ImportSkill(new PlannerSkill(kern
+        var plannerSkill = kernel.ImportSkill(new PlannerSkill(kernel));
+
+        // Act
+        var context = await kernel.RunAsync("<someTag>" + GoalText, plannerSkill["ExecutePlan"]);
+
+        // Assert
+        var plan = context.Variables.ToPlan();
+        Assert.NotNull(plan);
+        Assert.NotNull(plan.Id);
+        Assert.Equal(string.Empty, plan.Goal);
+        Assert.Equal("<someTag>" + GoalText, plan.PlanString);
+        Assert.False(plan.IsSuccessful);
+        Assert.True(plan.IsComplete);
+        Assert.Contains("Failed to parse plan xml.", plan.Result, StringComparison.OrdinalIgnoreCase);
+    }
+
+    //
+    // Advanced tests for ExecutePlan that use mock sk functions to test the flow
+    //
+    [Theory]
+    [InlineData("Test the functionFlowRunner", @"<goal>Test the functionFlowRunner</goal>
+<plan>
+<function.MockSkill.Echo input=""Hello World"" />
+</plan>")]
+    public async Task ExecutePlanCanCallFunctionAsync(string goalText, string planText)
+    {
+        // Arrange
+        var kernel = KernelBuilder.Create();
+        _ = kernel.Config.AddOpenAICompletionBackend("test", "test", "test");
+        var plannerSkill = kernel.ImportSkill(new PlannerSkill(kernel));
+        _ = kernel.ImportSkill(new MockSkill(this._testOutputHelper), "MockSkill");
+        Plan createdPlan = new()
+        {
+            Goal = goalText,
+            PlanString = planText
+        };
+
+        // Act
+        var context = await kernel.RunAsync(createdPlan.ToJson(), plannerSkill["ExecutePlan"]);
+
+        // Assert
+        var plan = context.Variables.ToPlan();
+        Assert.NotNull(plan);
+        Assert.NotNull(plan.Id);
+        Assert.Equal(goalText, plan.Goal);
+        Assert.True(plan.IsSuccessful);
+        Assert.True(plan.IsComplete);
+        Assert.Equal("Echo Result: Hello World", plan.Result, true);
+    }
+
+    // Test that contains a #text node in the plan
+    [Theory]
+    [InlineData("Test the functionFlowRunner", @"<goal>Test the functionFlowRunner</goal>
+<plan>
+<function.MockSkill.Echo input=""Hello World"" />
+This is some text
+</plan>")]
+    public async Task ExecutePlanCanCallFunctionWithTextAsync(string goalText, string planText)
+    {
+        // Arrange
+        var kernel = KernelBuilder.Create();
+        _ = kernel.Config.AddOpenAICompletionBackend("test", "test", "test");
+        var plannerSkill = kernel.ImportS
