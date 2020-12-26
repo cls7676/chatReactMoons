8633ac8b663d07f92d0938b1cafcfe2d233bef7d
cls7676 <118163362+cls7676@users.noreply.github.com>
2020-12-26 16:12:14 +0000

    Moar changes

--- samples/apps/auth-api-webapp-react/src/components/InteractWithGraph.tsx
+++ samples/apps/auth-api-webapp-react/src/components/InteractWithGraph.tsx
@@ -15,4 +15,55 @@ interface IData {
 
 const InteractWithGraph: FC<IData> = ({ uri, config, onBack }) => {
     const sk = useSemanticKernel(uri);
-    const defaultText = `A glacier is a persistent body of dense ice that is constantly moving under its own weight. A glacier forms where the accumulati
+    const defaultText = `A glacier is a persistent body of dense ice that is constantly moving under its own weight. A glacier forms where the accumulation of snow exceeds its ablation over many years, often centuries. It acquires distinguishing features, such as crevasses and seracs, as it slowly flows and deforms under stresses induced by its weight. As it moves, it abrades rock and debris from its substrate to create landforms such as cirques, moraines, or fjords. Although a glacier may flow into a body of water, it forms only on land and is distinct from the much thinner sea ice and lake ice that form on the surface of bodies of water.`;
+    const filename = 'AuthenticationSampleSummary.docx';
+    const path = '%temp%\\' + filename;
+    const destinationPath = '/' + filename;
+
+    const [text, setText] = React.useState(defaultText);
+
+    const runTask1 = async () => {
+        try {
+            //get summary
+            var summary = await sk.invokeAsync(config, { value: text }, 'summarizeskill', 'summarize');
+
+            //write document
+            await sk.invokeAsync(
+                config,
+                {
+                    value: summary.value,
+                    inputs: [{ key: 'filePath', value: path }],
+                },
+                'documentskill',
+                'appendtextasync',
+            );
+
+            //upload to onedrive
+            await sk.invokeAsync(
+                config,
+                {
+                    value: path,
+                    inputs: [{ key: 'destinationPath', value: destinationPath }],
+                },
+                'clouddriveskill',
+                'uploadfileasync',
+            );
+        } catch (e) {
+            alert('Something went wrong.\n\nDetails:\n' + e);
+        }
+    };
+
+    const runTask2 = async () => {
+        try {
+            var shareLink = await sk.invokeAsync(
+                config,
+                { value: destinationPath },
+                'clouddriveskill',
+                'createlinkasync',
+            );
+            var myEmail = await sk.invokeAsync(config, { value: '' }, 'emailskill', 'getmyemailaddressasync');
+
+            await sk.invokeAsync(
+                config,
+                {
+                    value: `Here's the link: ${shareLink.value}\n\nReminder: Please delete the document on your OneDrive after you finish w
