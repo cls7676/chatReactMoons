c708a6cd9affd8e32c8af6fac5c5ef2388c484b5
cls7676 <118163362+cls7676@users.noreply.github.com>
2022-01-12 05:58:52 +0000

    Updated README file.

--- samples/apps/book-creator-webapp-react/src/components/CreateBook.tsx
+++ samples/apps/book-creator-webapp-react/src/components/CreateBook.tsx
@@ -25,4 +25,67 @@ import { IAsk, IAskInput } from '../model/Ask';
 import { IKeyConfig } from '../model/KeyConfig';
 import { IBookResult, IPage } from '../model/Page';
 
-inter
+interface IData {
+    uri: string;
+    title: string;
+    description: string;
+    keyConfig: IKeyConfig;
+    onBack: () => void;
+}
+
+interface IProcessHistory {
+    uri: string;
+    functionName: string;
+    input: string;
+    timestamp: string;
+}
+
+enum BookCreationState {
+    Ready = 0,
+    GetNovelOutline = 1,
+    GetSummaryOfOutline = 2,
+    ReadyToCreateBookFromOutline = 3,
+    CreateBookFromOutline = 4,
+    Complete = 5,
+}
+
+const CreateBook: FC<IData> = ({ uri, title, description, keyConfig, onBack }) => {
+    const sk = useSemanticKernel(uri);
+    const [bookCreationState, setBookCreationState] = useState<BookCreationState>(BookCreationState.Ready);
+    const [busyMessage, setBusyMessage] = useState<string>('');
+    const [bookState, setBookState] = useState<IBookResult>({} as IBookResult);
+    const [showProcess, setShowProcess] = useState<boolean>(false);
+    const [processHistory, setProcessHistory] = useState<IProcessHistory[]>([]);
+
+    useEffect(() => {
+        const runAsync = async () => {
+            switch (bookCreationState) {
+                case BookCreationState.GetNovelOutline:
+                    setBusyMessage('Creating novel outline');
+                    await runNovelOutlineFunction();
+                    setBusyMessage('');
+                    break;
+                case BookCreationState.GetSummaryOfOutline:
+                    setBusyMessage('Getting summary');
+                    await runSummariseFunction();
+                    setBusyMessage('');
+                    break;
+                case BookCreationState.CreateBookFromOutline:
+                    setBusyMessage('Creating an 8 page book');
+                    await runCreateBookFunction();
+                    setBusyMessage('');
+                    break;
+            }
+        };
+
+        runAsync();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [bookCreationState]);
+
+    useEffect(() => {
+        switch (bookCreationState) {
+            case BookCreationState.GetNovelOutline:
+                setBookCreationState(BookCreationState.GetSummaryOfOutline);
+                break;
+            case BookCreationState.GetSummaryOfOutline:
+                setBookCreationState(BookCreationState.ReadyToCreateBookFro
