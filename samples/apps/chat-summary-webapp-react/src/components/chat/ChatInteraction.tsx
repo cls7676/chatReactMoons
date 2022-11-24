3127ba317c05cbd050a256989a14b6ccc939243a
cls7676 <118163362+cls7676@users.noreply.github.com>
2022-11-24 20:29:09 +0000

    Incorporate review comments

--- samples/apps/chat-summary-webapp-react/src/components/chat/ChatInteraction.tsx
+++ samples/apps/chat-summary-webapp-react/src/components/chat/ChatInteraction.tsx
@@ -25,4 +25,48 @@ const ChatInteraction: FC<IData> = ({ uri, onGetAISummary, onBack }) => {
         <div style={{ paddingTop: 20, gap: 20, display: 'flex', flexDirection: 'column', alignItems: 'stretch' }}>
             <Title3 style={{ alignItems: 'left' }}>Interact with Chat</Title3>
             <Body1>
-                This is not a
+                This is not an interactive chat session. Modify the chat contents by editing ChatThread.ts - you can
+                also add to the chat by typing a message below and pressing enter.
+            </Body1>
+
+            <div
+                style={{
+                    position: 'relative',
+                    maxHeight: '70vh',
+                    display: 'grid',
+                    gridTemplateColumns: 'minmax(300px, 800px)',
+                    gridTemplateRows: 'auto 1fr auto',
+                    gridTemplateAreas: "'menu' 'content' 'footer'",
+                    justifyContent: 'center',
+                    padding: '0 1rem',
+                }}
+            >
+                <div style={{ gridArea: 'content', overflowY: 'auto', overflowX: 'hidden', padding: '1rem 0' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+                        {chatHistory.map((m, idx) => (
+                            <ChatHistoryItem key={idx} message={m} />
+                        ))}
+                        <div ref={chatBottomRef} />
+                    </div>
+                </div>
+                <div style={{ gridArea: 'footer', padding: '1rem 0', borderTop: '1px solid #ccc' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
+                        <ChatInput
+                            onSubmit={(m) => {
+                                setIsBusy(true);
+                                setChatHistory([...chatHistory, m]);
+                                setIsBusy(false);
+                            }}
+                        />
+                        <Body1>Finished chatting? Next, get your AI Summary.</Body1>
+                        <div style={{ display: 'flex', flexDirection: 'row', gap: 20 }}>
+                            <Button style={{ width: 54 }} appearance="secondary" onClick={() => onBack()}>
+                                Back
+                            </Button>
+                            <Button
+                                style={{ width: 175 }}
+                                appearance="primary"
+                                onClick={() => onGetAISummary(chatHistory)}
+                            >
+                                Get AI Summary
+                       
