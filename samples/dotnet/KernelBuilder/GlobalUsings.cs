84923204fb83da348a50107e5be895f13c65073e
cls7676 <118163362+cls7676@users.noreply.github.com>
2022-12-18 08:04:26 +0000

    Back to 7.0-dev

---
+++ samples/dotnet/KernelBuilder/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Logging.Abstractions;
+global using Microsoft.SemanticKernel;
+global using Microsoft.SemanticKernel.AI;
+global using Microsoft.SemanticKernel.AI.OpenAI.Services;
+global using Microsoft.S
