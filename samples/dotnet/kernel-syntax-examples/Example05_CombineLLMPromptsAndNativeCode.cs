1b758e618a09ea902ed31cb9f468ee978762ba26
cls7676 <118163362+cls7676@users.noreply.github.com>
2023-01-02 23:05:51 +0000

    SONATA 5G NFV CMUD v2.1 (Create, Manage, Upgrade and Destroy) - versioning

---
+++ samples/dotnet/kernel-syntax-examples/Example05_CombineLLMPromptsAndNativeCode.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.KernelExtensions;
+using Microsoft.SemanticKernel.Skills.Web;
+using Microsoft.SemanticKernel.Skills.Web.Bing;
+using RepoUtils;
+
+// ReSharper disable once InconsistentNaming
+public static class Example05_CombineLLMPromptsAndNativeCode
+{
+    public static async Task RunAsync()
+    {
+        Console.WriteLine("======== LLMPrompts ========");
+
+        IKernel kernel = new KernelBuilder().WithLogger(ConsoleLogger.Log).Build();
+
+        // OpenAI settings
+        kernel.Config.AddOpenAICompletionBackend("text-davinci-002", "text-davinci-002", Env.Var("OPENAI_API_KEY"));
+        kernel.Config.AddOpenAICompletionBackend("text-davinci-003", "text-davinci-003", Env.Var("OPENAI_API_KEY"));
+        kernel.Config.SetDefaultCompletionBackend("text-davinci-003");
+
+        // Load native skill
+        using var bingConnector = new BingConnector(Env.Var("BING_API_KEY"));
+        var bing = new WebSearchEngineSkill(bingConnector);
+        var search = kernel.ImportSkill(bing, "bing");
+
+        // Load semantic skill defined with prompt templates
+        string folder = RepoFiles.SampleSkillsPath();
+
+        var sumSkill = kernel.ImportSemanticSkillFromDirectory(
+            folder,
+            "SummarizeSkill");
+
+        // Run
+        var ask = "What's the tallest building in South America?";
+
+        var result1 = await kernel.RunAsync(
+            ask,
+            search["SearchAsync"]
+ 
