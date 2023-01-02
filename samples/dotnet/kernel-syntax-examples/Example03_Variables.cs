37a447d08a56f8dc10a1a97cd1cb476d26fea625
cls7676 <118163362+cls7676@users.noreply.github.com>
2023-01-02 19:51:57 +0000

    ASoC: wm8994: Fix enum ctl accesses in a wrong type

---
+++ samples/dotnet/kernel-syntax-examples/Example03_Variables.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
+using RepoUtils;
+using Skills;
+
+// ReSharper disable once InconsistentNaming
+public static class Example03_Variables
+{
+    private static readonly ILogger s
