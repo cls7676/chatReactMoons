66e9835030b4e3284cdeea425a0006f6aef14c69
cls7676 <118163362+cls7676@users.noreply.github.com>
2023-02-06 07:25:38 +0000

    f2fs: add a max block check for get_data_block_bmap

--- samples/dotnet/kernel-syntax-examples/Skills/TextSkill.cs
+++ samples/dotnet/kernel-syntax-examples/Skills/TextSkill.cs
@@ -22,4 +22,24 @@ public class TextSkill
     }
 
     [SKFunction("Remove spaces to the left and right of a string")]
-    [SKFun
+    [SKFunctionInput(Description = "Text to edit")]
+    public string Strip(string input)
+    {
+        return input.Trim();
+    }
+
+    [SKFunction("Change all string chars to uppercase")]
+    [SKFunctionInput(Description = "Text to uppercase")]
+    public string Uppercase(string input)
+    {
+        return input.ToUpperInvariant();
+    }
+
+    [SKFunction("Change all string chars to lowercase")]
+    [SKFunctionInput(Description = "Text to lowercase")]
+    [SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "By design.")]
+    public string Lowercase(string input)
+    {
+        return input.ToLowerInvariant();
+    }
+}
