2c0a9d11b3fc2112988bd98ee2889234b4488ad0
cls7676 <118163362+cls7676@users.noreply.github.com>
2023-02-06 06:35:57 +0000

    Fix missing newline

---
+++ samples/dotnet/kernel-syntax-examples/Skills/TextSkill.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.SemanticKernel.SkillDefinition;
+
+namespace Skills;
+
+public class TextSkill
+{
+    [SKFunction("Remove spaces to the left of a string")]
+    [SKFunctionInput(Description = "Text to edit")]
+    public string LStrip(string input)
+    {
+        return input.TrimStart();
+    }
+
+    [SKFunction("Remove spaces to the right of a string")]
+    [SKFunctionInput(Description = "Text to edit")]
+    public string RStrip(string input)
+    {
+        return input.TrimEnd();
+    }
+
+    [SKFunction("Remove spaces to the left and right of a string")]
+    [SKFun
